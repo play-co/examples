5bcec5fdb5665e9ee8dfa4b55b02b4279f2444de
Mario Balibrera <mario@gameclosure.com>
2013-08-09 18:29:36 -0700

    comments and screenshot

--- src/ui/toast/src/Application.js
+++ src/ui/toast/src/Application.js
@@ -1,31 +1,37 @@
+//# Toast: <a title="View raw file" href="https://raw.github.com/gameclosure/examples/master/src/ui/toast/src/Application.js"><img src="../../include/download_icon.png" class="icon"></a>
+//This is a demonstration of Toasts
 import ui.TextView as TextView;
 import ui.widget.Toast as Toast;
 
-exports = Class(GC.Application, function () {
-	var posIndex = -1;
-	var toastIndex = -1;
-	var positions = [
-		'top',
-		'topright',
-		'bottomright',
-		'bottom',
-		'bottomleft',
-		'topleft'
-	];
-	var toastWords = [
-		'something',
-		'another thing',
-		'good job',
-		'you did it'
-	];
+// test data
+var posIndex = -1;
+var toastIndex = -1;
+var positions = [
+	'top',
+	'topright',
+	'bottomright',
+	'bottom',
+	'bottomleft',
+	'topleft'
+];
+var toastWords = [
+	'something',
+	'another thing',
+	'good job',
+	'you did it'
+];
 
+//### Class: Application
+exports = Class(GC.Application, function () {
 	this.initUI = function () {
+		// create TextView that triggers a Toast popup on click
 		var textview = new TextView({
 			superview: this,
 			layout: 'box',
 			text: 'Toast?',
 			color: 'white'
 		});
+		// create the Toast
 		var toast = new Toast({
 			superview: this,
 			images: {
@@ -37,6 +43,7 @@ exports = Class(GC.Application, function () {
 				bottomleft: 'resources/images/left.png'
 			}
 		});
+		// create input handler that pops the Toast
 		textview.on('InputSelect', function() {
 			posIndex = (posIndex + 1) % 6;
 			toastIndex = (toastIndex + 1) % 4;
@@ -44,3 +51,6 @@ exports = Class(GC.Application, function () {
 		});
 	};
 });
+
+//The output should look like this screenshot:
+//<img src="./doc/screenshot.png" alt="a book screenshot" class="screenshot">
